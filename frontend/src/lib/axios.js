d10ebaa298601530bea8bc4a9550faf6eea220f9
momo <mohssinem32@gmail.com>
2025-08-20 16:33:34 +0100

    debugging on production environnement

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,19 @@
-import axios from "axios";
+/*import axios from "axios";
+//Developpement envirnnement 
+
 
 const axiosInstance = axios.create({
   // This was the issue - incorrect environment check and port number
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api", // ← VIRGULE ICI !
   withCredentials: true, // send cookies to the server
+});
+
+export default axiosInstance;*/
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL, // ex: https://e-commerce-project-production-c237.up.railway.app/api
+  withCredentials: true,
 });
 
 export default axiosInstance;
